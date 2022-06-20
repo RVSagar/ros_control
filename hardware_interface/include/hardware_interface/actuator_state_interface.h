a20736c58248318f2b8196bf8e18038da4b652c1
Sai Kishor Kothakota <sai.kishor@pal-robotics.com>
2022-06-20 18:20:09 +0200

    added a proper throw message for easy introspection

--- hardware_interface/include/hardware_interface/actuator_state_interface.h
+++ hardware_interface/include/hardware_interface/actuator_state_interface.h
@@ -76,10 +76,11 @@ public:
     {
       throw HardwareInterfaceException("Cannot create handle '" + name + "'. Effort data pointer is null.");
     }
-    if(pid_gains_ && (*pid_gains_).size() != 3)
+    if(pid_gains_ && pid_gains_->size() != 5)
     {
-      throw HardwareInterfaceException("Cannot create handle '" + name +
-                                       "'. The parsed PID gains pointer is not of size 3.");
+      throw HardwareInterfaceException(
+          "Cannot create handle '" + name + "'. The parsed PID gains pointer is of size: " +
+          std::to_string(pid_gains_->size()) + ". Expected size : 5");
     }
 
   }
